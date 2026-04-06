e2f7813ea39205f1dad348e132049818ca07766e
muuFisal <fisalmhmd56@gmail.com>
2026-04-06 12:23:53 +0200

    feat: implement CategoryController to handle paginated index and show requests for categories

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Helpers\ApiResponse;
+use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Commerce\CategoryIndexRequest;
 use App\Http\Resources\CategoryResource;
 use App\Services\Api\Commerce\CategoryService;
 use Illuminate\Support\Facades\Log;
 
-class CategoryController extends ApiController
+class CategoryController extends Controller
 {
     public function __construct(protected CategoryService $service) {}
 
@@ -27,13 +28,18 @@ class CategoryController extends ApiController
                 'per_page' => $categories->perPage(),
             ],
         ]);
-        return response()->json($categories);
-        // return ApiResponse::sendResponse(
-        //     200,
-        //     __('front.categories-retrieved-successfully'),
-        //     CategoryResource::collection($categories),
-        //     $this->paginationData($categories)
-        // );
+        return ApiResponse::sendResponse(
+            200,
+            __('front.categories-retrieved-successfully'),
+            CategoryResource::collection($categories),
+            [
+                'total' => $categories->total(),
+                'count' => $categories->count(),
+                'current_page' => $categories->currentPage(),
+                'last_page' => $categories->lastPage(),
+                'per_page' => $categories->perPage(),
+            ]
+        );
     }
 
     public function show(string $slug)
