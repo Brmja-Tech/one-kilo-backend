f502cea23c35dd94d14e8dd5d60c083689626a69
muuFisal <fisalmhmd56@gmail.com>
2026-04-06 12:26:32 +0200

    feat: implement CategoryController to handle paginated index and show requests

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,42 +3,25 @@
 namespace App\Http\Controllers\Api;
 
 use App\Helpers\ApiResponse;
-use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Commerce\CategoryIndexRequest;
 use App\Http\Resources\CategoryResource;
 use App\Services\Api\Commerce\CategoryService;
-use Illuminate\Support\Facades\Log;
 
-class CategoryController extends Controller
+class CategoryController extends ApiController
 {
-    public function __construct(protected CategoryService $service) {}
+    public function __construct(protected CategoryService $service)
+    {
+    }
 
     public function index(CategoryIndexRequest $request)
     {
         $categories = $this->service->paginate($request->filters());
 
-        Log::info('request', $request->all());
-        Log::info('categories', [
-            'items' => $categories->items(),
-            'pagination' => [
-                'total' => $categories->total(),
-                'count' => $categories->count(),
-                'current_page' => $categories->currentPage(),
-                'last_page' => $categories->lastPage(),
-                'per_page' => $categories->perPage(),
-            ],
-        ]);
         return ApiResponse::sendResponse(
             200,
             __('front.categories-retrieved-successfully'),
             CategoryResource::collection($categories),
-            [
-                'total' => $categories->total(),
-                'count' => $categories->count(),
-                'current_page' => $categories->currentPage(),
-                'last_page' => $categories->lastPage(),
-                'per_page' => $categories->perPage(),
-            ]
+            $this->paginationData($categories)
         );
     }
 
