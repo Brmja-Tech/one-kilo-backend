51a62b639e8b8d383a5bd6b1bab66f0121f08f67
muuFisal <fisalmhmd56@gmail.com>
2026-04-06 12:09:46 +0200

    feat: implement CategoryController to handle index and show requests for commerce categories

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -6,17 +6,17 @@ use App\Helpers\ApiResponse;
 use App\Http\Requests\Api\Commerce\CategoryIndexRequest;
 use App\Http\Resources\CategoryResource;
 use App\Services\Api\Commerce\CategoryService;
+use Illuminate\Support\Facades\Log;
 
 class CategoryController extends ApiController
 {
-    public function __construct(protected CategoryService $service)
-    {
-    }
+    public function __construct(protected CategoryService $service) {}
 
     public function index(CategoryIndexRequest $request)
     {
         $categories = $this->service->paginate($request->filters());
 
+        Log::info('categories', $categories);
         return response()->json($categories);
         // return ApiResponse::sendResponse(
         //     200,
